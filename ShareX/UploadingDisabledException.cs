88a0ba7c1d5b7b382fde041d90c35e1ffaed7736
gnalvesteffer <xorberax@gmail.com>
2022-08-23 20:47:35 -0500

    throw exception when UploadManager operation occurs

---
+++ ShareX/UploadingDisabledException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ShareX
+{
+    internal class UploadingDisabledException : InvalidOperationException
+    {
+        public UploadingDisabledException()
+        {
+            MessageBox.Show("UploadManager operation prevented.", "UPLOAD PREVENTED", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+        }
+    }
+}
